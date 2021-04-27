9068b3e1395b05cfce1f5cb50c3f2a5c625a3697
luanlvt <fptnet1280@yahoo.com>
2021-04-27 09:10:45 -0500

    add

---
+++ exercises/js/index25.js
@@ -0,0 +1,13 @@
+var superHeroName = 'Batman';
+var realName = 'Bruce Wayne';
+var fly = false
+var hasBatMobile = true;
+var life = 90;
+var freeTime = null;
+
+console.log('superHeroName', superHeroName, typeof(superHeroName));
+console.log('realname',realName, typeof(realName));
+console.log('fly',fly, typeof(fly));
+console.log('hasBatMobile', hasBatMobile, typeof(hasBatMobile));
+console.log('life',life, typeof(life));
+console.log('freeTime',freeTime,typeof(freeTime));
